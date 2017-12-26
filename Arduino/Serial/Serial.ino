8f01af973d4bf20a2df4c6719d6a0831b85ee4b9
Rootar <mokrosinski95@gmail.com>
2017-12-27 00:58:20 +0100

    Dodanie kodu potrzebnego do testowania komunikacji między RP a Ardu

---
+++ Arduino/Serial/Serial.ino
@@ -0,0 +1,25 @@
+#include <LiquidCrystal.h>
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  Serial1.begin(115200);
+  Serial.begin(115200);
+
+  lcd.begin(16, 2);
+}
+
+char dane;
+
+void loop() {
+  if(Serial.available() > 0){
+    dane = Serial.read();
+    Serial1.write(dane);
+  }
+
+  if(Serial1.available() > 0){
+    dane = Serial1.read();
+    Serial.print(dane);
+    lcd.print(dane);
+  }  
+}
