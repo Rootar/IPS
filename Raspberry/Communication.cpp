4f41d7875a640f4efb1b944bf3061dd5d16e3bf7
Artur Mokrosiński <mokrosinski95@gmail.com>
2018-02-02 14:37:28 +0100

    again num

--- Raspberry/Communication.cpp
+++ Raspberry/Communication.cpp
@@ -8,7 +8,6 @@
 
 Communication::Communication(const char* portname, int speed, int timeout)
 : connection(portname, speed, timeout){
-    num = 0;
 }
 
 int Communication::csend(const unsigned char* message, uint8_t size){
